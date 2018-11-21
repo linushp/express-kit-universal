5934ac5587588c25f17494fed4b0d6beddc78c89
luanhaipeng <luanhaipeng@staff.beechat.io>
2018-11-21 14:00:41 +0800

    window

--- dist/ExpressKitUniversal.js
+++ dist/ExpressKitUniversal.js
@@ -173,7 +173,8 @@ module.exports = eku_compile_template;
 
 var eku_compile_template = __webpack_require__(0);
 
-var __$windowObj = {};
+var __REGISTER_COMPONENT_CONTAINER__ = {};
+var __HTML2JS_TPL__ = "__HTML2JS_TPL__";
 
 function isObject(x) {
     return Object.prototype.toString.call(x) === "[object Object]";
@@ -181,6 +182,10 @@ function isObject(x) {
 
 function isFunction(x) {
     return Object.prototype.toString.call(x) === "[object Function]";
+}
+
+function isString(x) {
+    return Object.prototype.toString.call(x) === "[object String]";
 }
 
 function extend_object(objA, objB) {
@@ -193,38 +198,55 @@ function extend_object(objA, objB) {
     return objA;
 }
 
+/**
+ *
+ * @param obj
+ * @param base_component 字符串
+ */
 function handle_base_component(obj, base_component) {
     if (base_component && base_component.length > 0) {
         var base_component_define = get_component(base_component);
         if (base_component_define) {
-            handle_base_component(obj, base_component_define.base_component_name);
+            handle_base_component(obj, base_component_define.base_component);
             extend_object(obj, base_component_define);
         }
     }
 }
 
-var register_component_container = {};
-
+/**
+ * @param componentName
+ * @param componentDefine 可以是对象，也可以是函数
+ * @returns {*}
+ */
 function register_component(componentName, componentDefine) {
-    componentDefine["$render"] = function (tpl_or_compiled, data) {
-        data = data || {};
-        var windowObj = __$windowObj;
-        handle_base_component(data, componentDefine.base_component_name);
-        extend_object(data, componentDefine);
 
-        if (isFunction(tpl_or_compiled)) {
-            return tpl_or_compiled(data, windowObj);
-        }
-        return eku_compile_template(tpl_or_compiled)(data, windowObj);
-    };
-    register_component_container[componentName] = componentDefine;
+    if (isObject(componentDefine)) {
+
+        componentDefine["$render"] = function (tpl_or_compiled, data) {
+            data = data || {};
+            var windowObj = __REGISTER_COMPONENT_CONTAINER__;
+            handle_base_component(data, componentDefine.base_component);
+            extend_object(data, componentDefine);
+
+            if (isFunction(tpl_or_compiled)) {
+                return tpl_or_compiled(data, windowObj);
+            }
+
+            return eku_compile_template(tpl_or_compiled)(data, windowObj);
+        };
+    }
+
+    __REGISTER_COMPONENT_CONTAINER__[componentName] = componentDefine;
     return componentDefine;
 }
 
 function register_component_by_template(componentName, html2js_tpl) {
     var componentDefine = {};
+    var componentDefineAttrCount = 0;
 
     var templateKeyBegin = componentName + "----";
+
+    var base_component = null;
 
     for (var templateKey in html2js_tpl) {
         if (html2js_tpl.hasOwnProperty(templateKey) && templateKey.indexOf(templateKeyBegin) === 0) {
@@ -239,6 +261,10 @@ function register_component_by_template(componentName, html2js_tpl) {
                 var tpl_compiled = eku_compile_template(tpl);
 
                 var propsMap = aaaaa.propsMap || {};
+
+                if (propsMap['base_component'] && propsMap['base_component'].length > 0) {
+                    base_component = propsMap['base_component'].trim();
+                }
 
                 var params = (propsMap['params'] || "").trim();
                 var params_array = params.split(",");
@@ -256,10 +282,17 @@ function register_component_by_template(componentName, html2js_tpl) {
                     return this.$render(tpl_compiled, data);
                 };
             })(templateDefine);
+
+            componentDefineAttrCount++;
         }
     }
 
-    return register_component(componentName, componentDefine);
+    if (componentDefineAttrCount > 0) {
+        componentDefine.base_component = base_component;
+        return register_component(componentName, componentDefine);
+    } else {
+        return null;
+    }
 }
 
 /**
@@ -267,40 +300,60 @@ function register_component_by_template(componentName, html2js_tpl) {
  * @param html2js_tpl 可以为空
  * @returns {*}
  */
-function get_component(componentName, html2js_tpl) {
-    var ccc = register_component_container[componentName];
+function get_component(componentName) {
+    var ccc = __REGISTER_COMPONENT_CONTAINER__[componentName];
     if (ccc) {
         return ccc;
     }
 
+    var html2js_tpl = __REGISTER_COMPONENT_CONTAINER__[__HTML2JS_TPL__];
     if (html2js_tpl) {
         return register_component_by_template(componentName, html2js_tpl);
     }
 }
 
 function extend_component(componentObject, xxxObj) {
-    extend_object(componentObject, xxxObj);
+    if (isObject(componentObject)) {
+        extend_object(componentObject, xxxObj);
+    } else if (isString(componentObject)) {
+        var compObj = get_component(componentObject);
+        if (!isObject(compObj)) {
+            console.log("cannot find component of :" + componentObject, compObj);
+            throw new Error("cannot find component of :" + componentObject);
+        }
+        extend_object(compObj, xxxObj);
+    }
 }
 
-function set_ssr_window(windowObj) {
-    __$windowObj = windowObj;
+/**
+ * 同时注册多个组件
+ */
+function register_components(obj) {
+    var result = {};
+    for (var comp_name in obj) {
+        if (obj.hasOwnProperty(comp_name)) {
+            var comp_obj = obj[comp_name];
+            result[comp_name] = register_component(comp_name, comp_obj);
+        }
+    }
+    return result;
 }
 
-function get_ssr_window() {
-    return __$windowObj;
+function get_all_components() {
+    return __REGISTER_COMPONENT_CONTAINER__;
 }
 
-function extend_ssr_window(windowObj) {
-    extend_object(__$windowObj, windowObj);
+function set_html2js_tpl(xxx) {
+    __REGISTER_COMPONENT_CONTAINER__[__HTML2JS_TPL__] = xxx;
 }
 
 module.exports = {
-    set_ssr_window: set_ssr_window,
-    get_ssr_window: get_ssr_window,
-    extend_ssr_window: extend_ssr_window,
+    set_html2js_tpl: set_html2js_tpl,
     get_component: get_component,
+    get_all_components: get_all_components,
     extend_component: extend_component,
     register_component: register_component,
+    register_components: register_components,
     register_component_by_template: register_component_by_template
 };
 
--- src/eku_component.js
+++ src/eku_component.js
@@ -1,14 +1,21 @@
 var eku_compile_template = require("./eku_compile_template");
 
-var __$windowObj = {};
+
+var __REGISTER_COMPONENT_CONTAINER__ = {};
+var __HTML2JS_TPL__ = "__HTML2JS_TPL__";
+
+
 
 function isObject(x) {
     return Object.prototype.toString.call(x) === "[object Object]";
 }
 
-
 function isFunction(x) {
     return Object.prototype.toString.call(x) === "[object Function]";
+}
+
+function isString(x) {
+    return Object.prototype.toString.call(x) === "[object String]";
 }
 
 
@@ -23,40 +30,59 @@ function extend_object(objA, objB) {
 }
 
 
+/**
+ *
+ * @param obj
+ * @param base_component 字符串
+ */
 function handle_base_component(obj, base_component) {
     if (base_component && base_component.length > 0) {
         var base_component_define = get_component(base_component);
         if (base_component_define) {
-            handle_base_component(obj, base_component_define.base_component_name);
+            handle_base_component(obj, base_component_define.base_component);
             extend_object(obj, base_component_define);
         }
     }
 }
 
 
-var register_component_container = {};
-
+/**
+ * @param componentName
+ * @param componentDefine 可以是对象，也可以是函数
+ * @returns {*}
+ */
 function register_component(componentName, componentDefine) {
-    componentDefine["$render"] = function (tpl_or_compiled, data) {
-        data = data || {};
-        var windowObj = __$windowObj;
-        handle_base_component(data, componentDefine.base_component_name);
-        extend_object(data, componentDefine);
 
-        if (isFunction(tpl_or_compiled)) {
-            return tpl_or_compiled(data, windowObj);
-        }
-        return eku_compile_template(tpl_or_compiled)(data, windowObj);
-    };
-    register_component_container[componentName] = componentDefine;
+    if (isObject(componentDefine)) {
+
+        componentDefine["$render"] = function (tpl_or_compiled, data) {
+            data = data || {};
+            var windowObj = __REGISTER_COMPONENT_CONTAINER__;
+            handle_base_component(data, componentDefine.base_component);
+            extend_object(data, componentDefine);
+
+            if (isFunction(tpl_or_compiled)) {
+                return tpl_or_compiled(data, windowObj);
+            }
+
+            return eku_compile_template(tpl_or_compiled)(data, windowObj);
+        };
+
+    }
+
+    __REGISTER_COMPONENT_CONTAINER__[componentName] = componentDefine;
     return componentDefine;
 }
 
 
 function register_component_by_template(componentName, html2js_tpl) {
     var componentDefine = {};
+    var componentDefineAttrCount = 0;
 
     var templateKeyBegin = componentName + "----";
+
+    var base_component = null;
+
 
     for (var templateKey in html2js_tpl) {
         if (html2js_tpl.hasOwnProperty(templateKey) && templateKey.indexOf(templateKeyBegin) === 0) {
@@ -73,8 +99,13 @@ function register_component_by_template(componentName, html2js_tpl) {
 
                 var propsMap = aaaaa.propsMap || {};
 
+                if (propsMap['base_component'] && propsMap['base_component'].length > 0) {
+                    base_component = propsMap['base_component'].trim();
+                }
+
                 var params = (propsMap['params'] || "").trim();
                 var params_array = params.split(",");
+
 
                 return function () {
                     var args = Array.prototype.slice.apply(arguments);
@@ -91,10 +122,18 @@ function register_component_by_template(componentName, html2js_tpl) {
 
 
             })(templateDefine);
+
+            componentDefineAttrCount++;
         }
     }
 
-    return register_component(componentName, componentDefine)
+
+    if (componentDefineAttrCount > 0) {
+        componentDefine.base_component = base_component;
+        return register_component(componentName, componentDefine)
+    } else {
+        return null;
+    }
 }
 
 
@@ -103,42 +142,64 @@ function register_component_by_template(componentName, html2js_tpl) {
  * @param html2js_tpl 可以为空
  * @returns {*}
  */
-function get_component(componentName, html2js_tpl) {
-    var ccc = register_component_container[componentName];
+function get_component(componentName) {
+    var ccc = __REGISTER_COMPONENT_CONTAINER__[componentName];
     if (ccc) {
         return ccc;
     }
 
+    var html2js_tpl = __REGISTER_COMPONENT_CONTAINER__[__HTML2JS_TPL__];
     if (html2js_tpl) {
         return register_component_by_template(componentName, html2js_tpl);
     }
+
 }
 
 
 function extend_component(componentObject, xxxObj) {
-    extend_object(componentObject, xxxObj);
+    if (isObject(componentObject)) {
+        extend_object(componentObject, xxxObj);
+    }
+    else if (isString(componentObject)) {
+        var compObj = get_component(componentObject);
+        if (!isObject(compObj)) {
+            console.log("cannot find component of :" + componentObject, compObj);
+            throw new Error("cannot find component of :" + componentObject);
+        }
+        extend_object(compObj, xxxObj);
+    }
 }
 
 
-function set_ssr_window(windowObj) {
-    __$windowObj = windowObj;
+/**
+ * 同时注册多个组件
+ */
+function register_components(obj) {
+    var result = {};
+    for (var comp_name in obj) {
+        if (obj.hasOwnProperty(comp_name)) {
+            var comp_obj = obj[comp_name];
+            result[comp_name] = register_component(comp_name, comp_obj);
+        }
+    }
+    return result;
 }
 
-function get_ssr_window() {
-    return __$windowObj;
+
+function get_all_components() {
+    return __REGISTER_COMPONENT_CONTAINER__;
 }
 
-function extend_ssr_window(windowObj) {
-    extend_object(__$windowObj,windowObj);
+function set_html2js_tpl(xxx) {
+    __REGISTER_COMPONENT_CONTAINER__[__HTML2JS_TPL__] = xxx;
 }
-
 
 module.exports = {
-    set_ssr_window: set_ssr_window,
-    get_ssr_window: get_ssr_window,
-    extend_ssr_window:extend_ssr_window,
+    set_html2js_tpl: set_html2js_tpl,
     get_component: get_component,
+    get_all_components: get_all_components,
     extend_component: extend_component,
     register_component: register_component,
+    register_components: register_components,
     register_component_by_template: register_component_by_template
 };
